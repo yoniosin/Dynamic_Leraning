ba39f530372d2c3da3ecc3b4624964db43401dde
yonio <yoni.osin@gmail.com>
2018-05-12 11:08:50 +0300

    section 3

--- ex2/Q3.py
+++ ex2/Q3.py
@@ -2,7 +2,7 @@ import numpy as np
 import itertools as it
 
 P1 = np.array([[0, 1/2, 1/2], [2/3, 0, 1/3], [3/4, 1/4, 0]])
-P2 = np.array([[0, 1/8, 7/8], [1/2, 0, 1/2], [1/4, 3/4, 0]])
+P2 = np.array([[0, 1/8, 7/8], [1/2, 0, 1/2], [3/4, 1/4, 0]])
 P = {1: P1, 2: P2}
 
 R1 = np.array([0.2, 1, 0.5])
@@ -29,22 +29,38 @@ def calcRewardWrapper(path_list, init_prob):
     return sum(totReward) / len(path_list), max(totReward)
 
 
-def bestPath(steps, prob):
-    V = [0.7, 1, 0.5]
-    policy = []
-    for _ in range(steps - 1):
-        V += max(np.dot(V, P[1]), np.dot(V, P[2]))
+def maxVal(a, b):
+    if a > b:
+        return a, 1
+    else:
+        return b, 2
+
+
+def bestPath(steps):
+    V = np.empty((3,3))
+    policy = {0: [], 1: [], 2: []}
+    for i in range(3):
+        reward, action = maxVal(R1[i], R2[i])
+        V[0, i] = reward
+        policy[i].append(action)
+    for j in range(1, steps):
+        a1_addition = np.dot(P1, V[j-1, :])
+        a2_addition = np.dot(P2, V[j-1, :])
+        for i in range(3):
+            reward, action = maxVal(R1[i] + a1_addition[i], R2[i] + a2_addition[i])
+            V[j, i] = reward
+            policy[i].append(action)
+
+    return policy[0], V[2, 0]
 
 
 if __name__ == '__main__':
     init_prob = np.array([1, 0, 0])
-    a_list = np.array([2, 1, 2])
+    a_list = np.array([1, 1, 1])
 
     print('a. first reward: ' + str(calcReward(a_list, init_prob)))
 
     path_list = list(it.product([1, 2], repeat=stateNum))
     print('b. second reward: ' + str(calcRewardWrapper(path_list, init_prob)))
 
-
-
-
+    print(bestPath(3))
