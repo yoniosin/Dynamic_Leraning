4bab9005bfa0317be52784dac308419f01da28aa
NaamaPearl <naama.pearl@gmail.com>
2018-05-12 11:04:14 +0300

    best path

--- ex2/Q3.py
+++ ex2/Q3.py
@@ -2,7 +2,7 @@ import numpy as np
 import itertools as it
 
 P1 = np.array([[0, 1/2, 1/2], [2/3, 0, 1/3], [3/4, 1/4, 0]])
-P2 = np.array([[0, 1/8, 7/8], [1/2, 0, 1/2], [1/4, 3/4, 0]])
+P2 = np.array([[0, 1/8, 7/8], [1/2, 0, 1/2], [3/4, 1/4, 0]])
 P = {1: P1, 2: P2}
 
 R1 = np.array([0.2, 1, 0.5])
@@ -26,25 +26,34 @@ def calcRewardWrapper(path_list, init_prob):
     for path in path_list:
         totReward.append(calcReward(path, init_prob))
 
-    return sum(totReward) / len(path_list), max(totReward)
+    return sum(totReward) / len(path_list)
 
 
-def bestPath(steps, prob):
+def bestPath(steps):
     V = [0.7, 1, 0.5]
-    policy = []
-    for _ in range(steps - 1):
-        V += max(np.dot(V, P[1]), np.dot(V, P[2]))
+    policy = np.empty((steps, stateNum), np.uint)
+    policy[0, :] = [2, 1, 1]
+    for i in range(1, steps):
+        vOption = np.array([R[1] + np.dot(P[1], V), R[2] + np.dot(P[2], V)])
+        maxV = vOption.max(0)
+        a = vOption.argmax(0) + 1
+        V += maxV
+        policy[i, :] = a
+    return policy, V
 
 
 if __name__ == '__main__':
     init_prob = np.array([1, 0, 0])
     a_list = np.array([2, 1, 2])
 
-    print('a. first reward: ' + str(calcReward(a_list, init_prob)))
+    print('a. first reward: ' + "%.2f" % calcReward(a_list, init_prob))
 
     path_list = list(it.product([1, 2], repeat=stateNum))
-    print('b. second reward: ' + str(calcRewardWrapper(path_list, init_prob)))
+    print('b. second reward: ' + "%.2f" % calcRewardWrapper(path_list, init_prob))
 
+    [policy, V] = bestPath(3)
+    print('policy for each step is ' + '\n' + str(policy))
+    print('Average reward after 3 steps starting from each state is ' + np.array_str(V, precision=2))
 
 
 
