d584f2bf8e7d7079c02d64cd4c6d2ae28708bd05
NaamaPearl <naama.pearl@campus.technion.ac.il>
2018-06-10 16:28:17 +0300

    .

--- ex4/Planning.py
+++ ex4/Planning.py
@@ -24,6 +24,11 @@ def clear_bit(value, bit):
     return value & ~(1 << bit)
 
 
+def calcNextState(curr_state, selected_job):
+    bit_to_change = len(cost) - 1 - selected_job
+    next_state = clear_bit(curr_state, bit_to_change)
+    return  next_state
+
 def calcValueFunction(policy, gamma):
     policy = policy - 1
     pi_len = len(policy)
@@ -32,14 +37,37 @@ def calcValueFunction(policy, gamma):
 
     for curr_state, selected_job in enumerate(policy):
         P[curr_state, curr_state] = 1 - mu[selected_job]
-        bit_to_change = len(cost) - 1 - selected_job
-        next_state = clear_bit(curr_state, bit_to_change)
+        next_state = calcNextState(curr_state, selected_job)
         P[curr_state, next_state] += mu[selected_job]
         curr_state_bit = np.unpackbits(np.array([[curr_state]], dtype=np.uint8), axis=1)
         l[curr_state] += sum(curr_state_bit[0, -len(cost):]*cost)
 
     V = np.dot(np.linalg.inv(np.eye(pi_len)-gamma * P), l)
-    return V
+    return V, P
+
+
+def policyIteration(policy, gamma):
+    policy = policy - 1
+    nextV, P = calcValueFunction(policy, gamma)
+    conToNextIter = True
+    while conToNextIter:
+        prevV = nextV
+        nextV = np.zeros(len(nextV))
+        nextPolicy = np.zeros(len(policy))
+        for s, curr_state in enumerate(policy):
+            curr_s_bit = np.unpackbits(np.array([[curr_state]], dtype=np.uint8), axis=1)[0,-len(cost)]
+            possible_jobs = np.where(curr_s_bit == 1)
+            v_for_curr_state = np.zeros(len(possible_jobs))
+            for a_idx, action in enumerate(possible_jobs):
+                next_state = calcNextState(s, action)
+                v_for_curr_state[a_idx] = gamma * P[next_state, :] * V
+            nextV[s] = np.max(v_for_curr_state)
+            nextPolicy[s] = possible_jobs[np.argmax(v_for_curr_state)]
+        conToNextIter = np.equal(prevV, nextV)
+
+    return nextPolicy
+
+
 
 
 if __name__ == '__main__':
@@ -47,6 +75,9 @@ if __name__ == '__main__':
     V_c_max = calcValueFunction(c_max_policy, 0.9)
 
     plt.figure()
-    plt.stem(V_c_max)
+    plt.stem(range(1, 32), c_max_policy[1:])
+    plt.xlabel('state'), plt.ylabel('action')
     plt.show()
+
+    policyIteration(c_max_policy, 0.9)
     print('all done')
